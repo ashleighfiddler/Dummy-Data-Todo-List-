f1294954a578dd8ef7fe8049880516400b5cd5ec
ashleighfiddler <ashleigh.fiddler@gmail.com>
2020-05-24 18:04:38 -0500

    first commit

---
+++ filter.js
@@ -0,0 +1,22 @@
+// withinLimits(10, 99) // => true
+// withinLimits(21, 81) // => false
+
+let num1 = "";
+let num1 = "";
+
+const withinLimits(num1, num) => {
+
+    if ((num1 <= 20 && num1 >=0) && (num2 <= 100 && num2 >= 80)) {
+        return true; 
+    }   
+    else    {
+        return false;
+    }
+}
+
+
+
+withinLimits(10, 99) // => true
+withinLimits(21, 81) // => false
+
+console.log(result)
